0d2218487c56c3ee3a2936f3f4c4cc9f60804ce7
nana-47 <nana.oikawa@rakus-partners.co.jp>
2022-06-02 10:28:49 +0900

    仮でコミットしてます

--- src/main/resources/static/js/itemPrice.js
+++ src/main/resources/static/js/itemPrice.js
@@ -1,7 +1,13 @@
 $(function() {	
-	//デフォルトの金額を取得
+	//数量変更用にデフォルトの金額を取得
     var defaultPrice = $('#itemPrice').text();
-
+    
+    //初期表示用にデフォルトの金額を取得
+    var defaultPrice2 = $('#itemPrice').text();
+    	//初期表示の金額をカンマ区切りに変更
+    	defaultPrice2 = String(defaultPrice2).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+		$('#itemPrice').text(defaultPrice2);
+    
 	//数量が変更された場合
 	$('#selectItem').on('change', function() {
 		let num = $('#selectItem').val();
@@ -10,54 +16,46 @@ $(function() {
 		switch (num){
 			case '1':
 				newPrice = defaultPrice * 1;
-				$('#itemPrice').text(newPrice);
+				
 				break;
 			case '2':
 				newPrice = defaultPrice * 2;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '3':
 				newPrice = defaultPrice * 3;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '4':
 				newPrice = defaultPrice * 4;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '5':
 				newPrice = defaultPrice * 5;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '6':
 				newPrice = defaultPrice * 6;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '7':
 				newPrice = defaultPrice * 7;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '8':
 				newPrice = defaultPrice * 8;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '9':
 				newPrice = defaultPrice * 9;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '10':
 				newPrice = defaultPrice * 10;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '11':
 				newPrice = defaultPrice * 11;
-				$('#itemPrice').text(newPrice);
 				break;
 			case '12':
 				newPrice = defaultPrice * 12;
-				$('#itemPrice').text(newPrice);
 				break;
 		};
 		
+		//変更後の金額をカンマ区切りに変更
+		newPrice = String(newPrice).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+		$('#itemPrice').text(newPrice);
 	});		
    
 });
