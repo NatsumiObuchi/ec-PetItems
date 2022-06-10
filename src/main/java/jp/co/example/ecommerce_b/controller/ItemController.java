ab6afc227089921a3ec46cd6303366c9af68f4d9
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-06-10 12:12:22 +0900

    FavoriteControllerの追加

--- src/main/java/jp/co/example/ecommerce_b/controller/ItemController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/ItemController.java
@@ -1,7 +1,6 @@
 package jp.co.example.ecommerce_b.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -13,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jp.co.example.ecommerce_b.domain.Favorite;
 import jp.co.example.ecommerce_b.domain.Item;
 import jp.co.example.ecommerce_b.domain.Order;
 import jp.co.example.ecommerce_b.domain.Review;
@@ -256,129 +254,6 @@ public class ItemController {
 
 		return "item_list_pet";
 	}
-
-	/**
-	 * お気に入り登録する処理
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/favorite")
-	public String favorite(FavoriteListRegisterForm favoriteListRegisterForm, Model model) {
-		User user = (User) session.getAttribute("user");
-		Integer itemId = Integer.parseInt(favoriteListRegisterForm.getItemId());
-		if (user == null) {// ユーザの情報はないのでuserIdはセットできない
-			Favorite newFavorite = new Favorite();
-			newFavorite.setItemId(itemId);
-			Date now = new Date();
-			newFavorite.setFavoriteDate(now);
-			session.setAttribute("newFavorite", newFavorite);
-			System.out.println(newFavorite);
-			return favoriteListShow(model);
-		}
-		Integer userId = user.getId();
-		Favorite favorite = new Favorite();
-		// formのuserIdとitemIdからお気に入り登録情報を取得する
-		favorite = favoriteService.findByUserIdItemId(userId, itemId);
-
-		System.out.println(1111111);
-		System.out.println(favorite);
-		if (favorite == null) {
-			Favorite newFavorite = new Favorite();
-			newFavorite.setItemId(itemId);
-			newFavorite.setUserId(user.getId());
-			Date now = new Date();
-			newFavorite.setFavoriteDate(now);
-			favoriteService.insertFavorite(newFavorite);
-			System.out.println(2222222);
-			String message1 = "お気に入り登録が完了しました！";
-			model.addAttribute("message1", message1);
-		} else if (favorite != null) {// ユーザが既にお気に入り登録済の場合
-			String message = "既にお気に入り登録済です";
-			model.addAttribute("message", message);
-		}
-		return itemDetail(itemId, model);
-	}
-	
-	/**
-	 * ユーザ情報を取得できた後、 元々ユーザ登録していなかったユーザがログインした後にお気に入り登録される処理 &
-	 * 登録済ユーザがログインした際に追加でお気に入り登録される処理
-	 * 
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping("/favorite2")
-	public String favorite2(Model model) {
-		User user = (User) session.getAttribute("user");
-		Favorite newFavorite = (Favorite) session.getAttribute("newFavorite");
-		if (newFavorite == null) {
-			return favoriteListShow(model);
-		}
-		Favorite oldFavorite = favoriteService.findByUserIdItemId(user.getId(), newFavorite.getItemId());
-		newFavorite.setUserId(user.getId());// 取得できたuserIdをここでやっとセット
-
-		if (oldFavorite == null) {
-			favoriteService.insertFavorite(newFavorite);
-		} else {// 既に登録済であったユーザの処理
-			String message = "既に登録済の商品です";
-			model.addAttribute("message", message);
-		}
-
-		return favoriteListShow(model);
-	}
-
-	/**
-	 * お気に入りリストの表示
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/favoriteList")
-	public String favoriteListShow(Model model) {
-		User user = (User) session.getAttribute("user");
-		if (user == null) {
-			session.setAttribute("transitionSourcePage", "favoriteList");
-			return "forward:/user/toLogin3";
-		}
-		Integer userId = user.getId();
-		List<Favorite> favoriteList = favoriteService.favoriteAll(userId);
-
-		if (favoriteList == null) {// ユーザ登録済でもお気に入りがゼロの時
-			String message = "お気に入り登録はありません";
-			model.addAttribute("message", message);
-			session.setAttribute("favoriteList", null);
-			return "favorite_list";
-		}
-
-		session.setAttribute("favoriteList", favoriteList);
-		for (Favorite favorite : favoriteList) {
-			Integer itemId = favorite.getItemId();
-			System.out.println("itemId:" + itemId);
-			Item item = itemService.load(itemId);
-			favorite.setItem(item);
-		}
-		return "favorite_list";
-	}
-
-	/**
-	 * お気に入りリストの削除
-	 * 
-	 * @param userId
-	 */
-	@RequestMapping("/deleteFavorite")
-	public String deleteFavorite(String itemId, Model model) {
-		Integer id = Integer.parseInt(itemId);
-		System.out.println(id);
-		favoriteService.delete(id);
-
-		return favoriteListShow(model);
-	}
-
-//	/**
-//	 * 購入履歴を表示する
-//	 */
-//	@RequestMapping("/orderHistory")
-//	public String orderHistory() {
-//		return "order_history";
-//	}
 	
 	/**
 	 * @param userId orderをセッションスコープに格納する処理。
