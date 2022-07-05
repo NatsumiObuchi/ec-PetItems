45131d755a92fec33a38dfbc9d5260699935e462
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-07-05 16:43:24 +0900

    UserControllerTestを実行しいくつか成功した(FavoriteControllerも少々編集してしまったけど..)

--- src/test/java/jp/co/example/ecommerce_b/controller/FavoriteControllerTest.java
+++ src/test/java/jp/co/example/ecommerce_b/controller/FavoriteControllerTest.java
@@ -20,8 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
-import jp.co.example.ecommerce_b.domain.User;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class FavoriteControllerTest {
@@ -52,12 +50,11 @@ class FavoriteControllerTest {
 	@DisplayName("ログインせずにお気に入りリストに遷移した場合の遷移先とスコープの値を確認")
 	void testNonLoginFavorite() throws Exception {
 		MockHttpSession mockSession = new MockHttpSession();
-		User user = null;
-		mockSession.setAttribute("user", user);
-		MvcResult result = mvc.perform(get("/favoriteList").session(mockSession))
+//		User user = null;
+		mockSession.setAttribute("user", null);
+		MvcResult result = mvc.perform(get("/favorite/favoriteList").session(mockSession))
 				.andExpect(view().name("forward:/user/toLogin3")).andReturn();
 		HttpSession session = result.getRequest().getSession();
 		assertEquals("favoriteList", session.getAttribute("transitionSourcePage"));
 	}
-
 }
